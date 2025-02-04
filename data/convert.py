1b004fae24113032f2642c22507515ca49037fe0
Danilo-Lapa11 <danwanderleylapa@gmail.com>
2025-02-03 21:49:55 -0300

    pre vizualizaçao dos dados

--- data/convert.py
+++ data/convert.py
@@ -2,10 +2,14 @@ import pandas as pd
 from scipy.io import arff
 
 # Carregar o arquivo ARFF
-data, meta = arff.loadarff("data/phpvqZpLa.arff")
+data, meta = arff.loadarff('data/phpvqZpLa.arff')
 
 # Converter para DataFrame
 df = pd.DataFrame(data)
+
+# Converter colunas de bytes para string
+for col in df.select_dtypes([object]):  # Seleciona colunas do tipo objeto (possíveis bytes)
+    df[col] = df[col].str.decode('utf-8')
 
 # Salvar como CSV
 df.to_csv("diabetes.csv", index=False)
